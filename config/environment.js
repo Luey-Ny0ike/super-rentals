53c195a94b17394b1164f32176a30e1272629a4d
lewis nyoike <nyoikelewis@gmail.com>
2017-06-06 10:08:28 +0300

    added forms for users to input new rentals

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,7 @@
-/* eslint-env node */
-
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
+
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
@@ -10,41 +9,18 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyAdQphLBmB6BHwkk_0J8mpW358-6WWs5kM",
+      authDomain: "super-rentals-6bfd5.firebaseapp.com",
+      databaseURL: "https://super-rentals-6bfd5.firebaseio.com",
+      storageBucket: "super-rentals-6bfd5.appspot.com",
+    },
+
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+
     }
-  };
-
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-
-  }
-
-  return ENV;
-};
+}
